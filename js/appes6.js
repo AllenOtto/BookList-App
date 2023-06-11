3cd55e544a0ca0b62d7cd18d64afeb4af6d51a28
AllenOtto <kephamoturi@gmail.com>
2023-06-11 07:14:04 +0300

    Created a book class defining a book and a UI class defining where and how the book will be rendered on the frontend

---
+++ js/appes6.js
@@ -0,0 +1,23 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+    addBookToList(book) {
+        const list = document.getElementById('book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="" class="delete">X</a></td>
+        `;
+        
+        list.appendChild(row);
+    }
+    
+}
